c04faeb05323280784f3bf9a6696d9cbca47f83c
Brandon Brewer <brandonbrewer@quickenloans.com>
2021-12-02 11:04:49 -0500

    Minor changes to 202101

--- scripts/2021/aoc202101.py
+++ scripts/2021/aoc202101.py
@@ -8,12 +8,12 @@ https://adventofcode.com/2021/day/1
 # PART 1
 def part_01(puzzle_input):
     number_of_increases = -1
-    current = 0
+    current_depth = 0
 
-    for x in puzzle_input:
-        if x > current:
+    for depth in puzzle_input:
+        if depth > current_depth:
             number_of_increases += 1
-        current = x
+        current_depth = depth
 
     return number_of_increases
 
@@ -22,20 +22,20 @@ def part_01(puzzle_input):
 def part_02(puzzle_input):
     number_of_increases = -1
     sliding_sum = 0
-    current = 0
+    current_depth = 0
 
-    for x in range(len(puzzle_input) - 2):
-        sliding_sum = sum(puzzle_input[x:x+3])
-        if sliding_sum > current:
+    for i in range(len(puzzle_input) - 2):
+        sliding_sum = sum(puzzle_input[i:i+3])
+        if sliding_sum > current_depth:
             number_of_increases += 1
-        current = sliding_sum
+        current_depth = sliding_sum
 
     return number_of_increases
 
 
 # Get Input
 if __name__ == "__main__":
-    file = r"C:\Users\BBREWER2\Documents\MyDirectory\AdventOfCode\inputs\2021\aoc202101.txt"
+    file = r"..\..\inputs\2021\aoc202101.txt"
     puzzle_input = open(file, 'r').read().split('\n')
     puzzle_input = list(map(int, puzzle_input))
     
